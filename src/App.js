4f13219741336305075f76d18b2f569555472fc3
atefehhar <aharaeni@gmail.com>
2023-03-22 09:31:36 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/atefeh-haraeini/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Atefeh Haraeini
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/atefehhar/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/atefeh-haraeini/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Atefeh Haraeini
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/atefehhar/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
