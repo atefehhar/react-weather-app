1f1cd3be9ad20c588b8bff45d221607a4abe3ae8
atefehhar <aharaeni@gmail.com>
2023-03-22 08:51:08 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://www.linkedin.com/in/atefeh-haraeini/" target="_blank">
+          Atefeh Haraeini
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/atefehhar/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
