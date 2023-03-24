1b4e7c8724f7fe3dcd6493d6549afe2aa47f78e4
atefehhar <aharaeni@gmail.com>
2023-03-24 16:17:25 +0100

    change city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
+        <Weather defaultCity="Tokyo" />
         <footer>
           This project is coded by{" "}
           <a
